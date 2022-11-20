189d6cbc92ba043826edd93692297ce81762a4fd
Dhruv Gursahani <dhruv.gursahani@outlook.com>
2022-11-20 21:57:25 +0530

    feat: added structure and styles to the page

---
+++ components/Element.js
@@ -0,0 +1,8 @@
+export const Element = ({ className }) => {
+  return (
+    <div
+      style={{ backfaceVisibility: 'hidden', outline: '1px solid transparent' }}
+      className={`hidden md:block absolute  h-[50px] w-[300px] bg-white z-0 transform -skew-x-12 -rotate-12 ${className}`}
+    ></div>
+  )
+}
