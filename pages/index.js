5f6fb0ed6354a1068a8dcc13723c45993c01ea16
Dhruv Gursahani <dhruv.gursahani@outlook.com>
2022-11-20 22:12:53 +0530

    feat: setting up turbopack to work with tailwindcss

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
-import { Element } from '@/components/Element'
-import { Navbar } from '@/components/Navbar'
-import { Hero } from '@/components/Hero'
+import { Element } from '../components/Element'
+import { Navbar } from '../components/Navbar'
+import { Hero } from '../components/Hero'
 
 export default function Home() {
   return (
