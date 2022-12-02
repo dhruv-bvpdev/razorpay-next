7896af8ced828cbb3b49d693ac990c3c2d0ac026
Dhruv Gursahani <dhruv.gursahani@outlook.com>
2022-12-03 01:05:55 +0530

    feat: integrated payments flow

---
+++ pages/api/razorpay.js
@@ -0,0 +1,35 @@
+import Razorpay from 'razorpay'
+import shortid from 'shortid'
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const razorpay = new Razorpay({
+      key_id: process.env.RAZORPAY_KEY_ID,
+      key_secret: process.env.RAZORPAY_KEY_SECRET
+    })
+
+    const payment_capture = 1
+    const amt = 499
+    const currency = 'INR'
+    const options = {
+      amount: (amt * 100).toString(),
+      currency,
+      receipt: shortid.generate(),
+      payment_capture
+    }
+
+    try {
+      const response = await razorpay.orders.create(options)
+      res.status(200).json({
+        id: response.id,
+        currency: response.currency,
+        amount: response.amount
+      })
+    } catch (err) {
+      console.log(err)
+      res.status(400).json(err)
+    }
+  } else {
+    // Handle any other HTTP errors
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,57 @@ import { Navbar } from '../components/Navbar'
 import { Hero } from '../components/Hero'
 
 export default function Home() {
+  const makePayment = async () => {
+    const res = await initializeRazorpay()
+
+    if (!res) {
+      alert('Razorpay SDK Failed to load')
+      return
+    }
+
+    const data = await fetch('/api/razorpay', { method: 'POST' }).then(t =>
+      t.json()
+    )
+
+    var options = {
+      key: process.env.RAZORPAY_KEY_ID, // Enter the Key ID generated from the Dashboard
+      name: 'Dhruv Gursahani Pvt Ltd',
+      currency: data.currency,
+      amount: data.amount,
+      order_id: data.id,
+      description: 'Thank You for your test donation',
+      image: 'https://dhruvgursahani.vercel.app/favicon.ico',
+      handler: function (response) {
+        alert(response.razorpay_payment_id)
+        alert(response.razorpay_order_id)
+        alert(response.razorpay_signature)
+      },
+      prefill: {
+        name: 'Test Name',
+        email: 'testemail@gmail.com',
+        contact: '9999999999'
+      }
+    }
+
+    const paymentObject = new window.Razorpay(options)
+    paymentObject.open()
+  }
+
+  const initializeRazorpay = () => {
+    return new Promise(resolve => {
+      const script = document.createElement('script')
+      script.src = 'https://checkout.razorpay.com/v1/checkout.js'
+      script.onload = () => {
+        resolve(true)
+      }
+      script.onerror = () => {
+        resolve(false)
+      }
+
+      document.body.appendChild(script)
+    })
+  }
+
   return (
     <div>
       <Head>
@@ -14,7 +65,7 @@ export default function Home() {
 
       <main className="font-Inter h-screen overflow-auto bg-gradient-to-tr from-[#252B30] to-[#191C22]">
         <Navbar />
-        <Hero />
+        <Hero onClick={makePayment} />
         <Element className="bottom-10 bg-gradient-to-r from-green-500 to-green-700 antialiased" />
         <Element className="bottom-20 w-[110px] bg-gradient-to-r from-indigo-500 to-indigo-700 antialiased" />
         <Element className="top-40 left-10 w-[150px] bg-gradient-to-r from-purple-500 to-purple-700 antialiased" />
